82d4e00acb17fa17f407615797d4b64abba52c72
Mkarlos <Mkarlos@DESKTOP-OCD9MAK>
2020-12-11 15:40:06 +0300

    first commit

--- site/js/app.js
+++ site/js/app.js
@@ -19,7 +19,7 @@ Firstname.onclick = function()
     {
         if (num>=12) num = 1;
         else num++;
-        $ajaxUtils.sendGetRequest("https://tweekerapp19.herokuapp.com/users/" + num, function(response) {
+        $ajaxUtils.sendGetRequest("https://misadryga.herokuapp.com/table1/" + num, function(response) {
             const json = (JSON.parse(response.responseText))[0];
             Firstname.innerText = json.first_name + " " + json.last_name;
             Email.innerText = json.email;
